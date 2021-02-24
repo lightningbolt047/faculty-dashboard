decf9a65ab3493058027841e38411d0096795a5a
Sashank <sashankvisweshwaran@gmail.com>
2021-02-24 18:06:06 +0530

    Initial commit. Login auth complete

---
+++ models/userSchema.js
@@ -0,0 +1,49 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const userSchema=new Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    clgID:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    authToken:{
+        type:String,
+        required:true
+    },
+    secQuestion:{
+        type:String
+    },
+    secAnswer:{
+        type:String
+    },
+    wrongAttempts:{
+        type:Number,
+        required:true,
+        range: [0,10]
+    },
+    phoneNumber:{
+        type:String,
+    },
+    address:{
+        type:String,
+    },
+    email:{
+        type:String,
+        required: true
+    },
+    imagePath:{
+        type: String
+    },
+    facultyType:{
+        type: String,
+        required: true
+    }
+});
+
+var User=mongoose.model('User',userSchema);
+module.exports=User;
---
+++ routes/authRouter.js
@@ -0,0 +1,75 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const { post } = require('../app');
+
+const authRouter=express.Router();
+const User=require('../models/userSchema');
+
+authRouter.route('/')
+.post((req,res,next)=>{
+    User.find({})
+    .then((users)=>{
+        var userPresent=false;
+        for(var i=0;i<users.length;i++){
+            if(users[i].clgID==req.body.clgID){
+                userPresent=true;
+                if(users[i].wrongAttempts>=10){
+                    res.statusCode=403;
+                    res.json({
+                        "status":"Account locked",
+                        "remainingAttempts":0
+                    });
+                    return;
+                }
+                if(users[i].authToken==req.body.token){
+                    User.findByIdAndUpdate(users[i]._id,{
+                        $set:{'wrongAttempts':0}
+                    }).then((document)=>{
+                        res.statusCode=200;
+                        res.json({
+                            "status": "Login Success",
+                            "remainingAttempts": 10
+                        });
+                    });
+                    break;
+                }
+                else{
+                    User.findByIdAndUpdate(users[i]._id,{
+                        $set:{'wrongAttempts':users[i].wrongAttempts+1}
+                    }).then((document)=>{
+                        res.statusCode=403;
+                        res.json({
+                            "status": "Wrong Password",
+                            "remainingAttempts": 10-users[i].wrongAttempts-1
+                        });
+                    });
+                    break;
+                }
+            }
+        }
+        if(userPresent==false){
+            res.statusCode=404;
+            res.json({
+                "status": "No such clgID"
+            });
+        }
+    })
+})
+
+.put((req,res,next)=>{
+    User.create(req.body)
+    .then((doc)=>{
+        res.statusCode=200;
+        res.json(doc);
+    })
+})
+
+.patch((req,res,next)=>{
+    User.findByIdAndUpdate(req.body.id,{
+        $set:{"wrongAttempts":0}
+    }).then((doc)=>{
+        res.end("Updated");
+    })
+})
+
+module.exports=authRouter;
