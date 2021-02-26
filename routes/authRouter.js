a91b0468043928ecaa9f37cd06aaadbf644430cb
Sashank <sashankvisweshwaran@gmail.com>
2021-02-26 13:24:28 +0530

    A more efficient way to login if login from cookie is used

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -9,53 +9,86 @@ const User=require('../models/userSchema');
 
 authRouter.route('/')
 .post((req,res,next)=>{
-    User.find({})
-    .then((users)=>{
-        var userPresent=false;
-        for(var i=0;i<users.length;i++){
-            if(users[i].clgID==req.body.clgID){
-                userPresent=true;
-                if(users[i].wrongAttempts>=10){
+    if(req.body.loginType==="cookie"){
+        User.findById(req.body.dbID)
+        .then((user)=>{
+            if(user.authToken===req.body.authToken){
+                res.statusCode=200;
+                res.json({
+                    "status": "Login Success",
+                    "dbID": user._id,
+                    "remainingAttempts": 10
+                });
+                return;
+            }else{
+                User.findByIdAndUpdate(req.body.dbID,{
+                    $set:{'wrongAttempts':user.wrongAttempts+1}
+                }).then((document)=>{
                     res.statusCode=403;
                     res.json({
-                        "status":"Account locked",
-                        "remainingAttempts":0
+                        "status": "Wrong Password",
+                        "remainingAttempts": 10-users[i].wrongAttempts
                     });
-                    return;
-                }
-                if(users[i].authToken==req.body.authToken){
-                    User.findByIdAndUpdate(users[i]._id,{
-                        $set:{'wrongAttempts':0}
-                    }).then((document)=>{
-                        res.statusCode=200;
-                        res.json({
-                            "status": "Login Success",
-                            "remainingAttempts": 10
-                        });
-                    });
-                    break;
-                }
-                else{
-                    User.findByIdAndUpdate(users[i]._id,{
-                        $set:{'wrongAttempts':users[i].wrongAttempts+1}
-                    }).then((document)=>{
+                });
+            }
+        },(err)=>{
+            res.status=500;
+            res.json({
+                status:"Something went wrong"
+            });
+        });
+    }
+    if(req.body.loginType==="user"){
+        User.find({})
+        .then((users)=>{
+            var userPresent=false;
+            for(var i=0;i<users.length;i++){
+                if(users[i].clgID==req.body.clgID){
+                    userPresent=true;
+                    if(users[i].wrongAttempts>=10){
                         res.statusCode=403;
                         res.json({
-                            "status": "Wrong Password",
-                            "remainingAttempts": 10-users[i].wrongAttempts
+                            "status":"Account locked",
+                            "remainingAttempts":0
                         });
-                    });
-                    break;
+                        return;
+                    }
+                    if(users[i].authToken==req.body.authToken){
+                        User.findByIdAndUpdate(users[i]._id,{
+                            $set:{'wrongAttempts':0}
+                        }).then((document)=>{
+                            res.statusCode=200;
+                            res.json({
+                                "status": "Login Success",
+                                "dbID": users[i]._id,
+                                "remainingAttempts": 10
+                            });
+                        });
+                        break;
+                    }
+                    else{
+                        User.findByIdAndUpdate(users[i]._id,{
+                            $set:{'wrongAttempts':users[i].wrongAttempts+1}
+                        }).then((document)=>{
+                            res.statusCode=403;
+                            res.json({
+                                "status": "Wrong Password",
+                                "remainingAttempts": 10-users[i].wrongAttempts
+                            });
+                        });
+                        break;
+                    }
                 }
             }
-        }
-        if(userPresent==false){
-            res.statusCode=404;
-            res.json({
-                "status": "No such clgID"
-            });
-        }
-    })
+            if(userPresent==false){
+                res.statusCode=404;
+                res.json({
+                    "status": "No such clgID"
+                });
+            }
+        })
+    }
+    
 })
 
 .put((req,res,next)=>{
