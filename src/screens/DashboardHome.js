86aca355fa20eafae858f5d9692bfa18005eb7ce
Eswar2000 <v.eswarraman2000@gmail.com>
2021-03-01 17:35:01 +0530

    Tried Adding Sidebar

--- src/screens/DashboardHome.js
+++ src/screens/DashboardHome.js
@@ -1,4 +1,4 @@
-import { Box, IconButton } from '@material-ui/core';
+import { Box, IconButton, makeStyles } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -6,13 +6,32 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import LogoutIcon from '@material-ui/icons/ExitToApp';
 import {useHistory} from 'react-router-dom';
 import { useCookies } from 'react-cookie';
+import Drawer from '@material-ui/core/Drawer';
+import { useState } from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import List from '@material-ui/core/List';
+
 
 export default function DashboardHome(){
 
+    const useStyles = makeStyles((theme)=>({
+        appbarBefore: {
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        }
+        
+    }));
+
     // eslint-disable-next-line
     const [cookies, setCookie, removeCookie] = useCookies(['faculty-dash-auth']);
+    const [sidebarOpen, setSidebarOpen]  = useState(false);
     const history=useHistory();
-
+    var iconText = ["Icon 1","Icon 2","Icon 3","Icon 4","Icon 5"];
+    var icons = [<MenuIcon/>,<LogoutIcon/>,<MenuIcon/>,<LogoutIcon/>,<MenuIcon/>];
 
     const logoutRoutine=()=>{
         removeCookie("dbID");
@@ -25,7 +44,7 @@ export default function DashboardHome(){
             <AppBar className="dashboardHomeAppBar" position="static">
                 <Toolbar>
                     <Box display="flex" flexGrow={1}>
-                        <IconButton edge="start" color="inherit">
+                        <IconButton edge="start" color="inherit" onClick={()=>console.log("Menu Clicked")}>
                             <MenuIcon/>
                         </IconButton>
                         <h2>Home</h2>
@@ -38,6 +57,16 @@ export default function DashboardHome(){
                     </IconButton>
                 </Toolbar>
             </AppBar>
+            <Drawer variant='permanent'>
+                    <List>
+                        {iconText.map((text, index) => (
+                        <ListItem button key={text}>
+                        <ListItemIcon>{icons[index]}</ListItemIcon>
+                        <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                    </List>
+            </Drawer>
         </div>
     );
 }
