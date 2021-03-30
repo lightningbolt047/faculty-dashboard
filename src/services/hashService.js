f128917fb8a1f0194392e2c63a43f844b0b0c30c
Sashank <sashankvisweshwaran@gmail.com>
2021-03-30 15:49:27 +0530

    Remove hash console.log

--- src/services/hashService.js
+++ src/services/hashService.js
@@ -3,6 +3,5 @@ const crypto=require('crypto');
 export default function hashString(username,password){
     const salt="~`!@#$%^&*()_0123456789";
     let hash=crypto.createHash('sha512').update(username+salt+password).digest('hex');
-    console.log("Hash "+hash);
     return hash;
 }
