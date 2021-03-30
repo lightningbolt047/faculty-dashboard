ec1ac9ccf65bcbff8e431c5893b0274f747cc80b
Sashank <sashankvisweshwaran@gmail.com>
2021-03-30 14:13:57 +0530

    Fix some warnings and changed gitignore

--- src/services/hashService.js
+++ src/services/hashService.js
@@ -2,7 +2,7 @@ const crypto=require('crypto');
 
 export default function hashString(username,password){
     const salt="~`!@#$%^&*()_0123456789";
-    var hash=crypto.createHash('sha512').update(username+salt+password).digest('hex');
+    let hash=crypto.createHash('sha512').update(username+salt+password).digest('hex');
     console.log("Hash "+hash);
     return hash;
 }
