42381855c2f24ade09181625319b75c0330fc25e
Sashank <sashankvisweshwaran@gmail.com>
2021-03-30 11:58:50 +0530

    Changed hash fn and salt

--- src/services/hashService.js
+++ src/services/hashService.js
@@ -1,8 +1,8 @@
 const crypto=require('crypto');
 
 export default function hashString(username,password){
-    const salt="~`!@#$%^&*()_";
-    var hash=crypto.createHash('sha1').update(username+salt+password).digest('hex');
+    const salt="~`!@#$%^&*()_0123456789";
+    var hash=crypto.createHash('sha512').update(username+salt+password).digest('hex');
     console.log("Hash "+hash);
     return hash;
 }
