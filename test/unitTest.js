a8e0ccb846f2b72ea818cf0d91050e133d46fd7b
Sashank <sashankvisweshwaran@gmail.com>
2021-04-08 09:12:31 +0530

    Changed unitTest describe statements

--- test/unitTest.js
+++ test/unitTest.js
@@ -29,7 +29,7 @@ describe('Unit testing the /auth route login with username password', function()
 
 describe('Unit testing the /auth route login with username password', function() {
 
-    it('should return OK status', function() {
+    it('should return 401 status', function() {
         return request(app)
             .post('/auth')
             .send({"loginType":"user","authToken":"csuhdfsdf","clgID":"cb.en.cse05001"})
@@ -41,7 +41,7 @@ describe('Unit testing the /auth route login with username password', function()
 
 describe('Unit testing the /auth route login with username password', function() {
 
-    it('should return OK status', function() {
+    it('should return 401 status', function() {
         return request(app)
             .post('/auth')
             .send({"loginType":"user","authToken":"csuhdfsdf","clgID":"cb.en.cse02001"})
@@ -53,7 +53,7 @@ describe('Unit testing the /auth route login with username password', function()
 
 describe('Unit testing the /auth route login with username password', function() {
 
-    it('should return OK status', function() {
+    it('should return 401 status', function() {
         return request(app)
             .post('/auth')
             .send({"loginType":"user","authToken":"c84c15cf188a1a18fe127241892afd145373f5ed7b4dbc0144d40ccdf5ca737e546db8d9273b43f49850388c3a1c3aacd6e5fa3557fc52fc7b362e591b965a68","clgID":"cb.en.cse05001"})
@@ -77,7 +77,7 @@ describe('Unit testing the /auth route login with username password', function()
 
 describe('Unit testing the /images route', function() {
 
-    it('should return OK status', function() {
+    it('should return 404 status', function() {
         return request(app)
             .get('/images/')
             .send()
@@ -89,7 +89,7 @@ describe('Unit testing the /images route', function() {
 
 describe('Unit testing the /auth route cookie', function() {
 
-    it('should return OK status', function() {
+    it('should return 401 status', function() {
         return request(app)
             .post('/auth')
             .send({"loginType":"cookie","authToken":"04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37117","dbID":"605a1bdd3dd7450a04455d6a"})
@@ -101,7 +101,7 @@ describe('Unit testing the /auth route cookie', function() {
 
 describe('Unit testing the /auth route login with username password', function() {
 
-    it('should return OK status', function() {
+    it('should return 401 status', function() {
         return request(app)
             .post('/auth')
             .send({"loginType":"user","authToken":"04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37117","clgID":"cb.en.cse01002"})
@@ -113,7 +113,7 @@ describe('Unit testing the /auth route login with username password', function()
 
 describe('Unit testing the /auth route login with username password', function() {
 
-    it('should return OK status', function() {
+    it('should return 404 status', function() {
         return request(app)
             .post('/auth')
             .send({"loginType":"user","authToken":"04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37117","clgID":"cb.en.cse010"})
@@ -125,7 +125,7 @@ describe('Unit testing the /auth route login with username password', function()
 
 describe('Unit testing the /auth route cookie', function() {
 
-    it('should return OK status', function() {
+    it('should return 404 status', function() {
         return request(app)
             .post('/auth')
             .send({"loginType":"cookie","authToken":"04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37007","dbID":"605a1bdd3dd7450a04455d33"})
@@ -146,7 +146,7 @@ describe('Unit testing the /profile route ', function() {
       .then((response)=>{
         expect(response.statusCode === 200).to.be.true
       })
-        })
+    })
 
         
     it('should return OK status', function() {
@@ -158,9 +158,9 @@ describe('Unit testing the /profile route ', function() {
         .then((response)=>{
           expect(response.statusCode === 200).to.be.true
         })
-          })
+    })
 
-      it('should return OK status', function() {
+      it('should return 401 status', function() {
         return request(app)
         .get('/profile/getFullProfile')
         .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37337')
@@ -170,7 +170,7 @@ describe('Unit testing the /profile route ', function() {
           expect(response.statusCode === 401).to.be.true
         })
           })
-      it('should return OK status', function() {
+      it('should return 404 status', function() {
         return request(app)
         .get('/profile/getFullProfile')
         .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37007')
@@ -181,7 +181,7 @@ describe('Unit testing the /profile route ', function() {
         })
           })
 
-      it('should return OK status', function() {
+      it('should return 404 status', function() {
         return request(app)
         .get('/profile/getClgIDOnly')
         .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37007')
@@ -191,7 +191,7 @@ describe('Unit testing the /profile route ', function() {
           expect(response.statusCode === 404).to.be.true
         })
           })
-    it('should return OK status', function() {
+    it('should return 401 status', function() {
       return request(app)
       .get('/profile/getClgIDOnly')
       .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37117')
@@ -202,7 +202,7 @@ describe('Unit testing the /profile route ', function() {
       })
     })
 
-    it('should return OK status', function() {
+    it('should return 404 status', function() {
         return request(app)
             .post('/profile/')
             .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37117')
@@ -247,7 +247,7 @@ describe('Unit testing the /recovery route ', function() {
             })
       })
 
-      it('should return OK status', function() {
+      it('should return 404 status', function() {
         return request(app)
             .post('/recovery')
             .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37007')
@@ -258,7 +258,7 @@ describe('Unit testing the /recovery route ', function() {
             })
       })
 
-      it('should return OK status', function() {
+      it('should return 401 status', function() {
         return request(app)
             .post('/recovery')
             .send({"reqType":'secAnswerChangePassword',"authToken":'04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37007',"secAnswer":'Atlanta',"dbID":"605a1bdd3dd7450a04455d6a"})
@@ -295,7 +295,7 @@ describe('Unit testing the /mentoring get route', function() {
 
 describe('Unit testing the /mentoring get route', function() {
 
-    it('should return OK status', function() {
+    it('should return 404 status', function() {
         return request(app)
             .get('/mentoring')
             .set('authToken','caf7e2a236f4e145f8842e74bf6b58dc5d3074f1d6d8bf3af5942bea26401c6ffbe2c4a92eb353746f9360ca8bb3de4b354da2c3e4fc918fff7aa82ef094258a')
@@ -323,7 +323,7 @@ describe('Unit testing the /mentoring route ', function() {
 
 describe('Unit testing the /mentoring get route', function() {
 
-  it('should return OK status', function() {
+  it('should return 401 status', function() {
     return request(app)
         .get('/mentoring')
         .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37033')
@@ -337,7 +337,7 @@ describe('Unit testing the /mentoring get route', function() {
 
 describe('Unit testing the /mentoring get route', function() {
 
-  it('should return OK status', function() {
+  it('should return 404 status', function() {
     return request(app)
         .get('/mentoring')
         .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37007')
@@ -351,7 +351,7 @@ describe('Unit testing the /mentoring get route', function() {
 
 describe('Unit testing the /mentoring route ', function() {
 
-  it('should return OK status', function() {
+  it('should return 404 status', function() {
     return request(app)
         .post('/mentoring')
         .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37007')
@@ -365,7 +365,7 @@ describe('Unit testing the /mentoring route ', function() {
 
 describe('Unit testing the /mentoring route ', function() {
 
-  it('should return OK status', function() {
+  it('should return 400 status', function() {
     return request(app)
         .post('/mentoring')
         .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37007')
@@ -379,7 +379,7 @@ describe('Unit testing the /mentoring route ', function() {
 
 describe('Unit testing the /mentoring route ', function() {
 
-    it('should return OK status', function() {
+    it('should return 404 status', function() {
         return request(app)
             .post('/mentoring')
             .set('authToken','04b3d8d2f757c5c6bb3986ff0a43cf7de9b657888173f06c96832ec1091a929ed562e6d1924496e93b98e4d55c5009d5affbaf4654133e32d366255885c37007')
