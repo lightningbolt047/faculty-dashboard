7a3476a15f883b81e613d5eb0d400827215c3ffc
Eswar2000 <v.eswarraman2000@gmail.com>
2021-04-17 22:01:10 +0530

    forum ui done

--- src/components/CourseNotes.js
+++ src/components/CourseNotes.js
@@ -5,12 +5,29 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import backendService from "../services/backendService";
 import {useState,useEffect} from "react";
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+
 
 let courseNotesFromServer=[];
 
 export default function CourseNotes({course}){
-
+    const [open, setOpen] = useState(false);
     const [courseNotes,setCourseNotes]=useState([]);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
 
     const getCourseNotesFromServer=async ()=>{
         let responseBody=await backendService('GET',`/courseNotes/${course.courseID}`,
@@ -42,9 +59,30 @@ export default function CourseNotes({course}){
                 <CourseNotesAccordion note={item}/>
             ))}
             <Box height={10}/>
-            <Fab className="floatingBtns" color="secondary">
+            <Fab className="floatingBtns" color="secondary" onClick={handleClickOpen}>
                 <AddIcon/>
             </Fab>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle>
+                <Typography variant="h5" color="secondary">Add New Note</Typography>
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Enter date and corresponding notes to maintain your daily progress.
+                </DialogContentText>
+                <TextField variant="outlined" color="secondary" label="Date" InputLabelProps={{ shrink: true }} type="date" fullWidth/>
+                <Box height={10}/>
+                <TextField variant="outlined" color="secondary" label="Notes" type="text" fullWidth/>
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleClose} color="primary">
+                Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+                Add Note
+            </Button>
+            </DialogActions>
+        </Dialog>
         </div>
     );
 }
--- src/components/Forum.js
+++ src/components/Forum.js
@@ -5,13 +5,26 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import backendService from "../services/backendService";
 import {useEffect, useState} from "react";
-
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 
 export default function Forum({course}){
-
+    const [open, setOpen] = useState(false);
     const [forumPosts,setForumPosts]=useState([]);
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const getForumPostsFromServer=async ()=>{
         let responseBody=await backendService('GET',`/forum/${course.courseID}`,
@@ -77,9 +90,28 @@ export default function Forum({course}){
                 <ForumPostAccordion postIndex={index} post={item} voteClickHandler={voteClickHandler}/>
             ))}
             <Box height={10}/>
-            <Fab className="floatingBtns" color="secondary">
+            <Fab className="floatingBtns" color="secondary" onClick={handleClickOpen}>
                 <AddIcon/>
             </Fab>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle>
+                    <Typography variant="h5" color="secondary">Add New Post</Typography>
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Type out your post here so that same course handling faculties can interact.
+                    </DialogContentText>
+                    <TextField variant="outlined" color="secondary" label="Post" type="text" fullWidth/>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={handleClose} color="primary">
+                    Post
+                </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
