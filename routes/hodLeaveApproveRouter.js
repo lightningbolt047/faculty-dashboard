8abcfa39506704232f67614fc1ad955798fb3702
Sashank <sashankvisweshwaran@gmail.com>
2021-04-22 23:05:23 +0530

    Added the ability to return the number of pending faculty leave requests without actually populating

--- routes/hodLeaveApproveRouter.js
+++ routes/hodLeaveApproveRouter.js
@@ -8,7 +8,7 @@ const User=require('../models/userSchema');
 const checkCredentials=require('../services/checkCredentialsService');
 const getFacultyAttendance=require('../services/getFacultyAttendance');
 
-hodLeaveApproveRouter.route('/')
+hodLeaveApproveRouter.route('/:reqType')
     .get(checkCredentials,(req,res,next)=>{
         User.findById(req.headers['dbid'])
             .then((user)=>{
@@ -30,44 +30,76 @@ hodLeaveApproveRouter.route('/')
                             return;
                         }
                         let curDate=Date.now();
-                        let sendDocument=[];
-                        for(const leave of leaves){
-                            if(curDate > Date.parse(leave.arrivalTime)){
-                                continue;
+                        if(req.params.reqType==='getNumLeaves'){
+                            let count=0;
+                            for(const leave of leaves){
+                                if(curDate > Date.parse(leave.arrivalTime) || leave.leaveStatus!=="pending"){
+                                    continue;
+                                }
+                                count+=1;
                             }
-                            let facultyPersonalDetails=await User.findById(leave.facultyID);
-                            facultyPersonalDetails.authToken=undefined;
-                            facultyPersonalDetails.secQuestion=undefined;
-                            facultyPersonalDetails.secAnswer=undefined;
-                            facultyPersonalDetails.lastSuccessfulLogin=undefined;
-                            facultyPersonalDetails.accountLockTime=undefined;
-                            facultyPersonalDetails.wrongAttempts=undefined;
-                            facultyPersonalDetails.imagePath=undefined;
 
-                            try{
-                                sendDocument.push({
-                                    personalDetails:facultyPersonalDetails,
-                                    passDetails:{
-                                        departureTime:leave.departureTime,
-                                        arrivalTime:leave.arrivalTime,
-                                        passStatus:leave.leaveStatus,
-                                        reason:leave.reason,
-                                        passID:leave._id
-                                    },
-                                    attendanceDetails:await getFacultyAttendance(leave.facultyID)
-                                });
-                            }catch(e){
-                                res.statusCode=500;
-                                res.json({
-                                    status:'Internal Server Error'
-                                });
+                            res.statusCode=200;
+                            res.json({
+                                numLeaves:count
+                            });
+                            return;
+                        }else if(req.params.reqType==='getAllLeaves'){
+                            let sendDocument=[];
+                            for(const leave of leaves){
+                                if(curDate > Date.parse(leave.arrivalTime)){
+                                    continue;
+                                }
+                                let facultyPersonalDetails=await User.findById(leave.facultyID);
+                                facultyPersonalDetails.authToken=undefined;
+                                facultyPersonalDetails.secQuestion=undefined;
+                                facultyPersonalDetails.secAnswer=undefined;
+                                facultyPersonalDetails.lastSuccessfulLogin=undefined;
+                                facultyPersonalDetails.accountLockTime=undefined;
+                                facultyPersonalDetails.wrongAttempts=undefined;
+                                facultyPersonalDetails.imagePath=undefined;
+
+                                try{
+                                    sendDocument.push({
+                                        personalDetails:facultyPersonalDetails,
+                                        passDetails:{
+                                            departureTime:leave.departureTime,
+                                            arrivalTime:leave.arrivalTime,
+                                            passStatus:leave.leaveStatus,
+                                            reason:leave.reason,
+                                            passID:leave._id
+                                        },
+                                        attendanceDetails:await getFacultyAttendance(leave.facultyID)
+                                    });
+                                }catch(e){
+                                    res.statusCode=500;
+                                    res.json({
+                                        status:'Internal Server Error'
+                                    });
+                                }
                             }
+                            res.statusCode=200;
+                            res.json(sendDocument);
+                        }else{
+                            res.statusCode=400;
+                            res.json({
+                                status:"Bad Request"
+                            });
                         }
-                        res.statusCode=200;
-                        res.json(sendDocument);
+                    },(err)=>{
+                        res.statusCode=500;
+                        res.json({
+                            status:"Internal Server Error"
+                        });
                     })
+            },(err)=>{
+                res.statusCode=500;
+                res.json({
+                    status:"Internal Server Error"
+                });
             })
-    })
+    });
+hodLeaveApproveRouter.route('/')
     .post((req,res,next)=>{
        User.findById(req.headers['dbid'])
            .then((user)=>{
