89dd16f125db555959326d1e34a9b44d21ea332b
Sashank <sashankvisweshwaran@gmail.com>
2021-05-25 16:54:42 +0530

    Change to cloud backend

--- src/services/backendService.js
+++ src/services/backendService.js
@@ -1,14 +1,14 @@
 
 
-// const cloudBackendURL='http://ec2-35-175-181-113.compute-1.amazonaws.com:4000';
-const localBackendURL='http://localhost:4000';
+const cloudBackendURL='http://ec2-35-175-181-113.compute-1.amazonaws.com:4000';
+// const localBackendURL='http://localhost:4000';
 const backendService = async (reqType, endpoint, reqBody, authToken, dbID) => {
     const reqOptions={
         method:reqType,
         headers:{'Content-Type':'application/json','authtoken':authToken,'dbid':dbID},
         body:reqType==='GET'?null:JSON.stringify(reqBody)
     }
-    let response= await fetch(localBackendURL+endpoint,reqOptions);
+    let response= await fetch(cloudBackendURL+endpoint,reqOptions);
     let data=await response.json();
     data.statusCode=response.status;
     return data;
