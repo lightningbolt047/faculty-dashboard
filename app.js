a8b65b5c41c4e891e466e270f892d044c077683f
Sashank <sashankvisweshwaran@gmail.com>
2021-05-25 17:43:40 +0530

    Move db urls to .env

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 var cors=require('cors');
 require('dotenv').config();
 
-// const localDBURL='mongodb://127.0.0.1:27017/';
-const cloudDBURL='mongodb+srv://sashank:sashank123@faculty-dashboard.0mowq.mongodb.net/faculty-dashboard?retryWrites=true&w=majority';
 
 
 const authRouter=require('./routes/authRouter');
@@ -30,7 +28,7 @@ mongoose.set('useFindAndModify', false);
 const User=require('./models/userSchema');
 
 
-const dbConnect=mongoose.connect(cloudDBURL);
+const dbConnect=mongoose.connect(process.env.DB_URL);
 dbConnect.then((db)=>{
   console.log("DB connection successful");
 },(err)=>{
