25f662c65d424595855f72f9607bc2c7d2ef00a2
Sashank <sashankvisweshwaran@gmail.com>
2021-05-25 17:40:44 +0530

    Added .env to make testing and production backends seprate

--- src/services/backendService.js
+++ src/services/backendService.js
@@ -1,6 +1,6 @@
 
 
-const cloudBackendURL='http://ec2-35-175-181-113.compute-1.amazonaws.com:4000';
+// const cloudBackendURL='http://ec2-35-175-181-113.compute-1.amazonaws.com:4000';
 // const localBackendURL='http://localhost:4000';
 const backendService = async (reqType, endpoint, reqBody, authToken, dbID) => {
     const reqOptions={
@@ -8,7 +8,7 @@ const backendService = async (reqType, endpoint, reqBody, authToken, dbID) => {
         headers:{'Content-Type':'application/json','authtoken':authToken,'dbid':dbID},
         body:reqType==='GET'?null:JSON.stringify(reqBody)
     }
-    let response= await fetch(cloudBackendURL+endpoint,reqOptions);
+    let response= await fetch(window.env.BACKEND_URL+endpoint,reqOptions);
     let data=await response.json();
     data.statusCode=response.status;
     return data;
